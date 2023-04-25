df84630354f9f19175b958bac44c2d8a1dd0e0f1
Jyothsna_devi <mvjoshna2011@gmail.com>
2023-04-25 00:44:59 -0500

    Adding DI to specflow framework

---
+++ Nextzen.Enterprise.BDD/BaseTestObject.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace Nextzen.Enterprise.BDD
+{
+    public abstract class BaseObject
+    {
+        public BaseObject(IConfiguration configuration, 
+            ScenarioContext scenarioContext, 
+            FeatureContext featureContext) {
+            Configuration = configuration;
+            ScenarioContext = scenarioContext;
+            FeatureContext = featureContext;
+        }
+        public IConfiguration Configuration { get; set; }
+        public ScenarioContext ScenarioContext { get; set; }
+        public FeatureContext FeatureContext { get; set; }
+
+    }
+}
---
+++ Nextzen.Enterprise.BDD/SeleniumWebdriver/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using Nextzen.Enterprise.BDD;
+
+namespace OpenQA.Selenium
+{
+    public abstract class BasePage : BaseObject
+    {
+        TestContext Context { get; set; }
+        IWebDriver Driver => Context.Driver;
+        
+        public BasePage(TestContext context) : base(context.Configuration, context.ScenarioContext, context.FeatureContext)
+        {
+            Context = context;
+        }
+    }
+}
