ef1fd21916425ae55d04887575731de21894d9ad
Jyothsna_devi <mvjoshna2011@gmail.com>
2023-04-26 12:16:42 -0500

    Adding helpers

---
+++ Nextzen.Enterprise.BDD/Helpers/ScenarioContextHelper.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TechTalk.SpecFlow;
+
+public static class ScenarioContextHelper
+{
+    public static ScenarioContext RegisterChromeDriver(this ScenarioContext scenarioContext, 
+        int implicitTimeout=30, 
+        ChromeOptions? options = null) {
+        scenarioContext.ScenarioContainer.RegisterFactoryAs<IWebDriver>((scenarioContainer) => { 
+
+            ChromeDriver driver = (options == null) ? new ChromeDriver() : new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(implicitTimeout);
+            return driver;
+        });
+
+        return scenarioContext;
+    }
+
+    public static ScenarioContext RegisterConfiguration(this ScenarioContext scenarioContext) {
+        scenarioContext.ScenarioContainer.RegisterFactoryAs<IConfiguration>((scenarioContainer) => {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder().
+            SetBasePath(Directory.GetCurrentDirectory()).
+            AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+
+            return configBuilder.Build();
+        });
+        return scenarioContext;
+    }
+}
---
+++ Nextzen.Enterprise.BDD/SeleniumWebdriver/WebDriverHelper.cs
@@ -0,0 +1,43 @@
+﻿
+using OpenQA.Selenium.Support.UI;
+
+namespace OpenQA.Selenium;
+
+public static class WebDriverHelper
+{
+    public static bool highlightElements = true;
+    public const int defaultTimeout = 30;
+
+    public static IWebElement FindElement(
+        this IWebDriver Driver, 
+        By locator, 
+        int timeOutInSeconds = defaultTimeout, 
+        string? errorMessage = null) {
+
+
+        IWebElement? resultElement = null;
+        var currentImplicitTimeout = Driver.Manage().Timeouts().ImplicitWait;
+        Driver.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
+        WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeOutInSeconds));
+
+        try
+        {
+            //Finding the element
+            resultElement = wait.Until(x => x.FindElement(locator));
+        }
+        catch{
+            //Throwing error message
+            if (errorMessage != null) { throw new Exception(errorMessage); }
+            throw;
+        }
+        finally {
+            Driver.Manage().Timeouts().ImplicitWait = currentImplicitTimeout;
+        }
+
+        //Highlighting the elements
+        if (highlightElements) {
+            ((IJavaScriptExecutor)Driver).ExecuteScript("arguments[0].style.outline='3px solid #ff6347'", resultElement);
+        }
+        return resultElement;
+    }
+}
